52296c7cc62c521ee018dcc78507e13c08f9ff6b
cleber.carvalho <contatoclbercarvalho@gmail.com>
2019-03-16 22:06:45 -0300

    Teste que verifica a relação de post com tags e autor

--- apps/api/tests/Unit/Models/PostTest.php
+++ apps/api/tests/Unit/Models/PostTest.php
@@ -9,6 +9,7 @@ use Illuminate\Foundation\Testing\DatabaseMigrations;
 
 use App\Post;
 use App\Tag;
+use App\Author;
 
 class PostTest extends TestCase
 {   
@@ -16,14 +17,27 @@ class PostTest extends TestCase
 
     public function testRelationshipWithTags()
     {   
-        $tag = factory(Tag::class)->make();
+        $tag = factory(Tag::class)->create();
+        $author = factory(Author::class)->create();
 
-        $post = factory(Post::class)->create();
+        $post = factory(Post::class)->create([
+            'author_id' => $author->id
+        ]);
+
         $post->tags()->save($tag);
-        
-        $p = $post->tags->toArray();
-        $t = $tag->toArray();
 
-        $this->assertEquals($tag->toArray(), $post->tags->toArray());
+        $tagIdInPost = $post->tags->first()->id;
+
+        $this->assertTrue(($tagIdInPost == $tag->id));
+    }
+
+    public function testRelationshipWithAuthor()
+    {   
+        $author = factory(Author::class)->create();
+        $post = factory(Post::class)->create([
+            'author_id' => $author->id
+        ]);
+
+        $this->assertTrue(($post->author->id == $author->id));
     }
 }
